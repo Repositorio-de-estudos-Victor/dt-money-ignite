f2c0373dda4f7c885328ae2630224393ec717465
Victor Novais <victornovais@MacBook-Pro-de-Victor.local>
2021-04-17 18:31:56 -0300

    Context API

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from "./styles/global";
+import { TransactionProvider } from './hooks/useTransactions';
 
 Modal.setAppElement('#root')
 
 export function App() {
 
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+  
 
   function handleOpenNewTransactionModal() {
     setIsNewTransactionModalOpen(true);
@@ -22,11 +24,14 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+      
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
+
       <GlobalStyle />
-    </>
+    </TransactionProvider>
   );
 }
